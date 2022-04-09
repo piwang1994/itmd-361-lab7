ed0728e0e5e2413e6e5e9a6059ed9f48f7cc7e1f
piwang1994 <1445207429@qq.com>
2022-04-09 21:46:11 +0800

    rename and formatted

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 function init() {
-    //add your javascrip between these two lines of code
+  //add your javascrip between these two lines of code
 
 
-    var h2center = document.querySelector("h2[class='center']")
-    h2center.textContent = "Qingbin Wang"
-    var h2center = document.querySelector("h3[class='center']")
-    h2center.textContent = "1445207429@qq.com"
+  var h2center = document.querySelector("h2[class='center']")
+  h2center.textContent = "Qingbin Wang"
+  var h2center = document.querySelector("h3[class='center']")
+  h2center.textContent = "1445207429@qq.com"
 
-    function inputAlert() {
-        var inputTxt = document.getElementById("entryinput").value
-        alert("Qingbin Wang:" + inputTxt)
-        if (inputTxt.length > 0) {
-            document.getElementById("textoutput").textContent = inputTxt
-        }
+  function inputAlert() {
+    var inputTxt = document.getElementById("entryinput").value
+    alert("Qingbin Wang:" + inputTxt)
+    if (inputTxt.length > 0) {
+      document.getElementById("textoutput").textContent = inputTxt
     }
-    document.getElementById("entrybutton").addEventListener("click", inputAlert)
+  }
+  document.getElementById("entrybutton").addEventListener("click", inputAlert)
 }
 
 
