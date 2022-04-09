416f964ca18cd5e3915f6eb95dca55c9148bc90e
piwang1994 <1445207429@qq.com>
2022-04-09 21:42:46 +0800

    OK

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-function init(){
-//add your javascrip between these two lines of code
- 
+function init() {
+    //add your javascrip between these two lines of code
 
 
+    var h2center = document.querySelector("h2[class='center']")
+    h2center.textContent = "Qingbin Wang"
+    var h2center = document.querySelector("h3[class='center']")
+    h2center.textContent = "1445207429@qq.com"
 
-
+    function inputAlert() {
+        var inputTxt = document.getElementById("entryinput").value
+        alert("Qingbin Wang:" + inputTxt)
+        if (inputTxt.length > 0) {
+            document.getElementById("textoutput").textContent = inputTxt
+        }
+    }
+    document.getElementById("entrybutton").addEventListener("click", inputAlert)
+}
 
 
 
